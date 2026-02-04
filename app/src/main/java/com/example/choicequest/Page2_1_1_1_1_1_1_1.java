d240d924a71498d0004ae7716609ea9a70bbf905
Eri Zen <erizenkeanu@gmail.com>
2026-02-04 12:23:25 +0800

    ily

--- app/src/main/java/com/example/choicequest/Page2_1_1_1_1_1_1_1.java
+++ app/src/main/java/com/example/choicequest/Page2_1_1_1_1_1_1_1.java
@@ -3,6 +3,7 @@ package com.example.choicequest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,34 +11,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Page2_1 extends AppCompatActivity {
+public class Page2_1_1_1_1_1_1_1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_page2_1);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.page2_1), (v, insets) -> {
+        setContentView(R.layout.activity_page2_1_1_1_1_1_1_1);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.page2_1_1_1_1_1_1_1), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Toast.makeText(this, "Market Monopoly Ending", Toast.LENGTH_LONG).show();
     }
-
-    public void page2_1_1(View view){
-        Intent i = new Intent(Page2_1.this, Page2_1_1.class);
-        startActivity(i);
-        finish();
-    }
-
-    public void page2_1_2(View view){
-        Intent i = new Intent(Page2_1.this, Page2_1_2.class);
-        startActivity(i);
-        finish();
-    }
-
-    public void page2_1_3(View view){
-        Intent i = new Intent(Page2_1.this, Page2_1_3.class);
+    public void title2(View view){
+        Intent i = new Intent(Page2_1_1_1_1_1_1_1.this, MainActivity.class);
         startActivity(i);
         finish();
     }
